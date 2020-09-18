4f069e3ead847f2d2ea4f4bec8bd006681fd53e0
vavehl <lorin@wmalumni.com>
2020-09-18 15:10:26 -0400

    Create foundation for deleting files from within the FileSearchListAdapter.

--- app/src/main/java/com/radelec/reconmobile/FileSearchListAdapter.java
+++ app/src/main/java/com/radelec/reconmobile/FileSearchListAdapter.java
@@ -79,17 +79,19 @@ public class FileSearchListAdapter extends RecyclerView.Adapter<FileSearchListAd
             holder.itemView.setBackgroundColor(holder.itemView.getResources().getColor(R.color.colorIvoryText));
             holder.tvFileName.setTextColor(holder.itemView.getResources().getColor(R.color.colorPrimaryDark));
             holder.tvDateModified.setTextColor(holder.itemView.getResources().getColor(R.color.colorPrimaryDark));
-            //holder.ibTrashDelete.findViewById(R.id.ibTrashDelete);
-            //holder.ibTrashDelete.setImageResource(R.drawable.ic_trash_24dp); //...do we really need this? It will leech resources.
-            //holder.ibTrashDelete.setVisibility(View.VISIBLE);
-            //holder.ibTrashDelete.setFocusable(true);
+            holder.ibTrashDelete.findViewById(R.id.ibTrashDelete);
+            holder.ibTrashDelete.setVisibility(View.VISIBLE);
+            holder.ibTrashDelete.setFocusable(true);
+            holder.ibTrashDelete.setOnClickListener(v -> {
+                Log.d("FileSearchListAdapter","Delete icon pressed for selectedPos=" + selectedPos + "!");
+            });
         } else {
             holder.itemView.setBackgroundColor(holder.itemView.getResources().getColor(R.color.colorPrimaryDark));
             holder.tvFileName.setTextColor(holder.itemView.getResources().getColor(R.color.colorIvoryText));
             holder.tvDateModified.setTextColor(holder.itemView.getResources().getColor(R.color.colorIvoryText));
-            //holder.ibTrashDelete.findViewById(R.id.ibTrashDelete);
-            //holder.ibTrashDelete.setVisibility(View.INVISIBLE);
-            //holder.ibTrashDelete.setFocusable(false);
+            holder.ibTrashDelete.findViewById(R.id.ibTrashDelete);
+            holder.ibTrashDelete.setVisibility(View.INVISIBLE);
+            holder.ibTrashDelete.setFocusable(false);
         }
         holder.tvDateModified.setText(DateFormat.format("dd-MMM-yyyy hh:mm", element.getDateModified()));
         holder.tvFileName.setText(element.getFileName());
